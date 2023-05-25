8520cc99967e37c0574c294f6f1bdd9b8e70f09e
Akanksha Sutar <sutarakanksha1710@gmail.com>
2023-05-25 16:33:14 +0530

    Form

---
+++ HtmlHelpersExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using HtmlHelpersExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelpersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Home
+        htmlhelpersdatabaseEntities2 db = new htmlhelpersdatabaseEntities2();
+
+        [HttpPost]
+        public ActionResult InsertEmp(Employee emp)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Employees.Add(emp);
+               
+                return Content("Successfully Inserted");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
